66eb9e253a5b8e17af41aab65aec98cdb85bbd15
alperen buğra <alperenbugra@192.168.1.36>
2026-06-18 17:46:35 +0300

    düzenleme

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -19,12 +19,12 @@ import { getFirestore } from "firebase/firestore";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDeEA1SWEfQUixvRnXmrtkI6_Q2QsfsG9Y",
-  authDomain: "chat-app-345f7.firebaseapp.com",
-  projectId: "chat-app-345f7",
-  storageBucket: "chat-app-345f7.firebasestorage.app",
-  messagingSenderId: "602656565366",
-  appId: "1:602656565366:web:932368c40f847c7ab65757",
+  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
 };
 
 // Guard against re-initialization during Fast Refresh / multiple imports.
